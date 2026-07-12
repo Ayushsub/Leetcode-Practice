f31e567955899073ec0ea10534082c9012138010
Ayush Patel <121381019+Ayushsub@users.noreply.github.com>
2026-07-12 10:24:42 +0530

    Time: 2 ms (7.68%), Space: 19.3 MB (26.4%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -3,7 +3,7 @@ class Solution:
         ans=""
         while columnNumber:
             columnNumber-=1
-            ans=chr(columnNumber%26+65)+ans
+            ans=chr(columnNumber%26+ord('A'))+ans
             columnNumber//=26
         return ans
 #Adjust by -1 because Excel columns are 1-based.
