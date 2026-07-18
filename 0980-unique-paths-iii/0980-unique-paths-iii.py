d1239846d7879664ae687ed96df11c431972e3f9
Ayush Patel <121381019+Ayushsub@users.noreply.github.com>
2026-07-18 21:56:21 +0530

    Time: 11 ms (52.25%), Space: 19.5 MB (26.08%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]):
+        rows=len(grid)
+        cols=len(grid[0])
+        total=0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]!=-1:
+                    total+=1
+                if grid[i][j]==1:
+                    sr=i
+                    sc=j
+        result=0
+        def backtrack(r,c,visited):
+            nonlocal result #Use the result from the outer function, not create a new one
+            if(r<0 or c<0 or r>=rows or c>=cols):
+                return
+            if(grid[r][c]==-1):
+                return
+            if((r,c) in visited):
+                return
+            visited.add((r,c))
+            if(grid[r][c]==2):
+                if(len(visited)==total):
+                    result+=1
+            else:
+                backtrack(r+1,c,visited)
+                backtrack(r-1,c,visited)
+                backtrack(r,c+1,visited)
+                backtrack(r,c-1,visited)
+            visited.remove((r,c))
+        backtrack(sr,sc,set())
+        return result
