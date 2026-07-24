859a80e9914d72f474dc967d1312c3107858097b
Ayush Patel <121381019+Ayushsub@users.noreply.github.com>
2026-07-24 18:01:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (5.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,41 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-
-        pairs = {
-            ')': '(',
-            ']': '[',
-            '}': '{'
+        stack=[]
+        pairs={
+            ')':'(',
+            ']':'[',
+            '}':'{'
         }
-
         for ch in s:
             if ch in "([{":
                 stack.append(ch)
             else:
-                # we need to check the case of empty stack is firts go like '){}' 
-                if not stack or stack.pop() != pairs[ch]:
+                #we need to check the case of empty stack is firts go like '){}' 
+                if not stack or stack.pop()!=pairs[ch]:
                     return False
-        # also "((((((" cases as we checked at last
-        return len(stack) == 0
+        #also "((((((" cases as we checked at last
+        return len(stack)==0
 
 
 """
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-
+        stack=[]
         for ch in s:
-            if ch == '(':
+            if ch=='(':
                 stack.append(')')
-            elif ch == '[':
+            elif ch=='[':
                 stack.append(']')
-            elif ch == '{':
+            elif ch=='{':
                 stack.append('}')
             else:
-                if not stack or stack.pop() != ch:
+                if not stack or stack.pop()!=ch:
                     return False
-
         return not stack
-
-
 """
