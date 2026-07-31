3fd7caa56f41c0b7d419775f6a1697d927763c20
Ayush Patel <121381019+Ayushsub@users.noreply.github.com>
2026-08-01 00:11:40 +0530

    Time: 52 ms (79.06%), Space: 62.9 MB (29.99%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -16,3 +16,26 @@ class Solution:
             i+=1
             j-=1
         return ans
+
+
+# class Solution:
+#     def pairSum(self, head: Optional[ListNode]) -> int:
+#         slow=head
+#         fast=head
+#         while fast and fast.next:
+#             slow=slow.next
+#             fast=fast.next.next
+#         prev=None
+#         while slow:
+#             nxt=slow.next
+#             slow.next=prev
+#             prev=slow
+#             slow=nxt
+#         ans=0
+#         first=head
+#         second=prev
+#         while second:
+#             ans=max(ans,first.val+second.val)
+#             first=first.next
+#             second=second.next
+#         return ans
