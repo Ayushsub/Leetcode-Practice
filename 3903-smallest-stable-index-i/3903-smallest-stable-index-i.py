f9b47bbdfc2fcc6e01efccd089f631e2fe7f4d1d
Ayush Patel <121381019+Ayushsub@users.noreply.github.com>
2026-09-04 23:00:40 +0530

    Time: 7 ms (48.03%), Space: 19.4 MB (9.85%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        for i in range(len(nums)):
+            left_max=max(nums[:i+1])
+            right_min=min(nums[i:])
+            if left_max-right_min<=k:
+                return i
+        return -1
